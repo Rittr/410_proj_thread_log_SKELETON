5404123d54188e648760536ce65911a3cd236d6f
Chris Ritter <chrisritter2@gmail.com>
2021-04-10 12:41:20 -0400

    actually saved the work I did -_-

--- src/410_proj_thread_log.cpp
+++ src/410_proj_thread_log.cpp
@@ -13,30 +13,51 @@
 
 using namespace std;
 
+//NOTE: you should have no mutexes in this file
 //TODO linker errors?  Did you include the pthread library?   And set the proper dialect?
 //TODO declare globals
 
+Logger fileLogger(LOG_FILE); // console
+Logger consoleLogger(LOG_CONSOLE); // file
+
+int numThreads = thread::hardware_concurrency();
+vector<thread> threadVector;
+
 /***
- * 
- * TODO log info to both file and console (you can do this with 2 Logger objects)
+ * TODO log info to both file and console You can do this with 2 Logger objects
+ *            Note:  These logger objects cannot be local variables, they must be shared
+ *            amongst the threads. (why?)
  * TODO this function should run until main (or another thread) asks it to quit
  * @param info  to log
  * returns void
  */
 void fun(string info){
-
+	fileLogger.Log(info);
+	consoleLogger.Log(info);
 }
 int main() {
 	
 	//TODO start as many threads as you have cores (see std::thread::hardware_concurrency())
 	//TODO save these threads in a vector
 
+	for (int i=0; i<numThreads; i++){
+		// start thread
+		// add thread to vector
+		threadVector.push_back(thread(fun,i));
+	}
+
+
 	//TODO let threads run a bit (5 seconds)
 	this_thread::sleep_for(chrono::milliseconds(5000));
 	
 	//TODO ask them all to stop
 	
+
 	//TODO wait for all threads to finish
+	for (int i=0; i<numThreads; i++){
+			// stop thread
+			threadVector[i].join();
+		}
 	
 	return 0;
 }
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -7,3 +7,56 @@
 #include <iostream>
 #include "../includes/Logger.h"
 using namespace std;
+
+//class Logger {
+	/***
+	 * creates a logger object, if lt=FILE then log to the file given by fn
+	 * if lt=LOG_CONSOLE the send log info to console via cout
+	 * @param lt either LOG_FILE or LOG_CONSOLE
+	 * @param fn if above is LOG_FILE then this is where info logged to
+	 *           otherwise it is not used
+	 */
+	Logger::Logger(LOG_TYPE lt, string fn) {  //Logger(LOG_TYPE lt, std::string fn=FILENAME);
+		this->lt = lt;
+		this->fn = fn;
+		this->fs = (lt == LOG_FILE) ? ofstream(fn) : NULL;
+//		this->mtx = mutex;
+	}
+
+
+	/***
+	 * Information to log, either to a file or to console
+	 * @param info to log
+	 */
+	void Logger::Log(string info) {
+		// TODO: log info here
+		mtx.lock();
+		if (lt == LOG_FILE) {
+			fs << info << endl;
+		}
+		else {
+			cout << info << endl;
+		}
+		mtx.unlock();
+	}
+
+	/***
+		 * close any open streams
+		 */
+	Logger::~Logger(){
+		mtx.unlock();
+		if (fs.is_open()) {
+			fs.close();
+		}
+	}
+
+
+//	LOG_TYPE lt;
+//	//if using LOG_TYPE::LOG_FILE then fn is the file where info is written,
+//	//and fs is the stream used to access it.  Otherwise both ignored
+//	std::string fn;
+//	std::ofstream fs;
+//
+//	//used to make instance of this class threadsafe
+//	std::mutex mtx;
+//};
