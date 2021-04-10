39124cd543decc4f22a1ea681596d38be429ab08
Chris Ritter <chrisritter2@gmail.com>
2021-04-10 13:20:10 -0400

    added loop to fun(), changed output to incrementing chars for each thread

--- src/410_proj_thread_log.cpp
+++ src/410_proj_thread_log.cpp
@@ -1,9 +1,9 @@
 //============================================================================
 // Name        : 410_proj_thread_log.cpp
-// Author      : 
-// Version     :
+// Author      : Chris Ritter
+// Version     : 1.0
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : example of threadsafe logging
 //============================================================================
 #include <thread>
 #include <iostream>
@@ -14,48 +14,50 @@
 using namespace std;
 
 //NOTE: you should have no mutexes in this file
-//TODO linker errors?  Did you include the pthread library?   And set the proper dialect?
-//TODO declare globals
+//linker errors?  Did you include the pthread library?   And set the proper dialect?
+//declare globals
 
 Logger fileLogger(LOG_FILE); // console
 Logger consoleLogger(LOG_CONSOLE); // file
+
+bool run = true;
 
 int numThreads = thread::hardware_concurrency();
 vector<thread> threadVector;
 
 /***
- * TODO log info to both file and console You can do this with 2 Logger objects
+ * log info to both file and console You can do this with 2 Logger objects
  *            Note:  These logger objects cannot be local variables, they must be shared
  *            amongst the threads. (why?)
- * TODO this function should run until main (or another thread) asks it to quit
+ * this function runs until main asks it to quit
  * @param info  to log
  * returns void
  */
 void fun(string info){
-	fileLogger.Log(info);
-	consoleLogger.Log(info);
+	while (run) {
+		fileLogger.Log(info);
+		consoleLogger.Log(info);
+	}
 }
 int main() {
 	
-	//TODO start as many threads as you have cores (see std::thread::hardware_concurrency())
-	//TODO save these threads in a vector
+	//start as many threads as you have cores (see std::thread::hardware_concurrency())
+	//save these threads in a vector
 
 	for (int i=0; i<numThreads; i++){
-		// start thread
-		// add thread to vector
-		threadVector.push_back(thread(fun,i));
+		char threadInfo = 'a'+i;
+		threadVector.push_back(thread(fun, string(5, threadInfo)));
 	}
 
 
-	//TODO let threads run a bit (5 seconds)
+	//let threads run a bit (5 seconds)
 	this_thread::sleep_for(chrono::milliseconds(5000));
 	
-	//TODO ask them all to stop
-	
+	//ask them all to stop
+	run = false;
 
-	//TODO wait for all threads to finish
+	//wait for all threads to finish
 	for (int i=0; i<numThreads; i++){
-			// stop thread
 			threadVector[i].join();
 		}
 	
